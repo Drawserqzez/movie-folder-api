a8802d1de91be69183ca85fd35391e91b63b4a2e
Algot Holton <drawserqzez@gmail.com>
2021-01-24 17:36:06 +0100

    Add node & express

---
+++ index.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+
+app.get("/api/v1/", (req, res) => {
+    res.json(JSON.stringify([1, 2, 3]));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on ${port}...`));
